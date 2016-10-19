94626745e0035dde0f1318aca8c4adac31ec25c0
CyberDev <denzelnihlus@gmail.com>
2016-10-19 21:58:31 +0200

    Configuration
    Basics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+
+
+    // Event Handler
+    public delegate void OnGameStateChange(GameState state);
+    public event OnGameStateChange OnStateChanged;
+
+    private GameState _current;
+    public GameState current
+    {
+        get
+        {
+            if (_current == null)
+            {
+                _current = GameState.Pause;
+            }
+            return _current;
+        }
+        set
+        {
+            _current = value;
+            if (OnStateChanged != null)
+            {
+                OnStateChanged(_current);
+            }
+        }
+    }
+}
