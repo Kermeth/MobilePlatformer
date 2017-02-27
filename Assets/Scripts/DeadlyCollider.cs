18900274113ea67c855fd553cd41553bad8cc705
CyberDev <denzelnihlus@gmail.com>
2017-02-27 03:33:39 +0100

    deadly collider

---
+++ Assets/Scripts/DeadlyCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadlyCollider : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.GetComponent<Stats>()) {
+            collision.gameObject.GetComponent<Stats>().GetHurt(collision.gameObject.GetComponent<Stats>().maxLife);
+        }
+    }
+}
