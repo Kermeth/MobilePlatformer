029933c63277148b529b7e09da77ba8af4b51c0c
CyberDev <denzelnihlus@gmail.com>
2017-03-03 23:55:02 +0100

    AutoInteractable Object

---
+++ Assets/Scripts/AutoInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutoInteractable : MonoBehaviour {
+
+    public UnityEvent OnInteract;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        OnInteract.Invoke();
+    }
+}
